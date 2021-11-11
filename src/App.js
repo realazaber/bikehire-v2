c83a430fb92e680f97388d844969a1cb0ce6f8a3
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-11 22:36:58 +1100

    starting features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./css/main.css";
 
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import Features from "./components/Features";
+import Features from "./components/Features/Features";
 import Gallery from "./components/Gallery/Gallery";
 import Download from "./components/Download";
 import Contact from "./components/Contact";
---
+++ src/components/Features/Features.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Features() {
+    return(
+        <div id="features">
+            <div className="feature">
+                View records
+            </div>
+
+            <div className="feature">
+                Add to records
+            </div>
+            
+            <div className="feature">
+                Save record
+            </div>
+
+            <div className="feature">
+                Delete records
+            </div>
+        </div>
+    );
+}
+
+export default Features;
